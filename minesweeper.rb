b21c30d46c0ba68e35365fa8dddb9da0b8e9fad2
noahwiener <noah.wiener@gmail.com>
2015-08-28 16:56:12 -0700

    finished game, it works, no saving

--- minesweeper.rb
+++ minesweeper.rb
@@ -8,6 +8,42 @@ class MineSweeper
     @board = Board.new(size)
   end
 
+  def over?
+    lost? || won?
+  end
+
+  def lost?
+    board.bomb_revealed
+  end
+
+  def won?
+    board.grid.flatten.each do |tile|
+      return false unless (tile.revealed || tile.is_bomb)
+    end
+    true
+  end
+
+  def play
+    until over?
+      play_turn
+    end
+    puts "You win!" if won?
+    puts "You lose" if lost?
+  end
+
+  def play_turn
+    puts "what would you like to do? Enter 'save' or an answer in the form of 'r/f, num, num' "
+    move = gets.chomp.split(", ")
+    pos = [move[1].to_i, move[2].to_i]
+    if move[0] == "f"
+      flag(pos)
+    elsif move[0] == "r"
+      reveal(pos)
+    else
+      puts "invalid input"
+    end
+  end
+
   def reveal(pos)
     board.reveal(pos)
     board.display
